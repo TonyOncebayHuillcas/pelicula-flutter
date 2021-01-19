7cc745dcaa63ed35d7c78a5b812242f601f159b0
Tony Brhayams Oncebay Huillcas <tony.oncebay.huillcas@gmail.com>
2021-01-19 18:17:37 -0500

    agredando borde redondea al swiper

--- movies/lib/src/widget/card_swiper_widget.dart
+++ movies/lib/src/widget/card_swiper_widget.dart
@@ -7,20 +7,24 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _screenSize = MediaQuery.of(context).size;
+
     return Container(
       padding: EdgeInsets.only(top: 10),
-      width: double.infinity,
-      height: 400,
       child: Swiper(
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
         layout: SwiperLayout.STACK,
         itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            "http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              "http://via.placeholder.com/350x150",
+              fit: BoxFit.fill,
+            ),
           );
         },
         itemCount: 3,
-        itemWidth: 300,
         pagination: new SwiperPagination(),
         //control: new SwiperControl(),
       ),
